818fd8844fa4a93cd0b1555f49e87487aec3fb33
eloyhernandezh3@gmail.com <eloyhernandezh3@gmail.com>
2026-03-11 23:26:49 -0600

    Se agrego el formulario de detalle torneo

---
+++ ProyectoBD/DetalleTorneo.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace ProyectoBD
+{
+    partial class DetalleTorneo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            cmbTorneo = new ComboBox();
+            cmbEquipo = new ComboBox();
+            bttnInscribir = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            dgvDetalleTorneo = new DataGridView();
+            bttnModificar = new Button();
+            bttnEliminar = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvDetalleTorneo).BeginInit();
+            SuspendLayout();
+            // 
+            // cmbTorneo
+            // 
+            cmbTorneo.FormattingEnabled = true;
+            cmbTorneo.Location = new Point(12, 29);
+            cmbTorneo.Name = "cmbTorneo";
+            cmbTorneo.Size = new Size(261, 23);
+            cmbTorneo.TabIndex = 0;
+            cmbTorneo.SelectedIndexChanged += cmbTorneo_SelectedIndexChanged;
+            // 
+            // cmbEquipo
+            // 
+            cmbEquipo.FormattingEnabled = true;
+            cmbEquipo.Location = new Point(12, 74);
+            cmbEquipo.Name = "cmbEquipo";
+            cmbEquipo.Size = new Size(261, 23);
+            cmbEquipo.TabIndex = 1;
+            cmbEquipo.SelectedIndexChanged += cmbEquipo_SelectedIndexChanged;
+            // 
+            // bttnInscribir
+            // 
+            bttnInscribir.BackColor = SystemColors.ActiveCaption;
+            bttnInscribir.Location = new Point(12, 119);
+            bttnInscribir.Name = "bttnInscribir";
+            bttnInscribir.Size = new Size(79, 28);
+            bttnInscribir.TabIndex = 2;
+            bttnInscribir.Text = "Agregar";
+            bttnInscribir.UseVisualStyleBackColor = false;
+            bttnInscribir.Click += bttnInscribir_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Torneo";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 56);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Equipo";
+            // 
+            // dgvDetalleTorneo
+            // 
+            dgvDetalleTorneo.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dgvDetalleTorneo.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvDetalleTorneo.Location = new Point(12, 175);
+            dgvDetalleTorneo.MultiSelect = false;
+            dgvDetalleTorneo.Name = "dgvDetalleTorneo";
+            dgvDetalleTorneo.ReadOnly = true;
+            dgvDetalleTorneo.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvDetalleTorneo.Size = new Size(457, 258);
+            dgvDetalleTorneo.TabIndex = 5;
+            // 
+            // bttnModificar
+            // 
+            bttnModificar.BackColor = SystemColors.ActiveCaption;
+            bttnModificar.Location = new Point(110, 119);
+            bttnModificar.Name = "bttnModificar";
+            bttnModificar.Size = new Size(79, 28);
+            bttnModificar.TabIndex = 6;
+            bttnModificar.Text = "Modificar";
+            bttnModificar.UseVisualStyleBackColor = false;
+            bttnModificar.Click += bttnModificar_Click;
+            // 
+            // bttnEliminar
+            // 
+            bttnEliminar.BackColor = SystemColors.ActiveCaption;
+            bttnEliminar.Location = new Point(211, 119);
+            bttnEliminar.Name = "bttnEliminar";
+            bttnEliminar.Size = new Size(79, 28);
+            bttnEliminar.TabIndex = 7;
+            bttnEliminar.Text = "Eliminar";
+            bttnEliminar.UseVisualStyleBackColor = false;
+            bttnEliminar.Click += bttnEliminar_Click;
+            // 
+            // DetalleTorneo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(483, 445);
+            Controls.Add(bttnEliminar);
+            Controls.Add(bttnModificar);
+            Controls.Add(dgvDetalleTorneo);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(bttnInscribir);
+            Controls.Add(cmbEquipo);
+            Controls.Add(cmbTorneo);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "DetalleTorneo";
+            Text = "DetalleTorneo";
+            ((System.ComponentModel.ISupportInitialize)dgvDetalleTorneo).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox cmbTorneo;
+        private ComboBox cmbEquipo;
+        private Button bttnInscribir;
+        private Label label1;
+        private Label label2;
+        private DataGridView dgvDetalleTorneo;
+        private Button bttnModificar;
+        private Button bttnEliminar;
+    }
+}
---
+++ ProyectoBD/DetalleTorneo.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ProyectoBD
+{
+    public partial class DetalleTorneo : Form
+    {
+        public DetalleTorneo()
+        {
+            InitializeComponent();
+        }
+
+        private void DetalleTorneo_Load(object sender, EventArgs e)
+        {
+            CargarTorneos();
+            CargarEquipos();
+            CargarDetalleTorneo();
+
+            cmbTorneo.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmbEquipo.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        void CargarTorneos()
+        {
+            ClaseConexion con = new ClaseConexion();
+
+            using (SqlConnection conn = con.conectar())
+            {
+                conn.Open();
+
+                string query = "SELECT IdTorneo, NombreTorneo FROM Torneo";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                cmbTorneo.DataSource = dt;
+                cmbTorneo.DisplayMember = "NombreTorneo";
+                cmbTorneo.ValueMember = "IdTorneo";
+            }
+        }
+
+        private void dgvDetalleTorneo_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow fila = dgvDetalleTorneo.Rows[e.RowIndex];
+                cmbTorneo.SelectedValue = fila.Cells["IdTorneo"].Value;
+                cmbEquipo.SelectedValue = fila.Cells["IdEquipo"].Value;
+            }
+        }
+
+        void CargarEquipos()
+        {
+            ClaseConexion con = new ClaseConexion();
+
+            using (SqlConnection conn = con.conectar())
+            {
+                conn.Open();
+
+                string query = "SELECT IdEquipo, NombreEquipo FROM Equipo";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                cmbEquipo.DataSource = dt;
+                cmbEquipo.DisplayMember = "NombreEquipo";
+                cmbEquipo.ValueMember = "IdEquipo";
+            }
+        }
+
+
+
+        private void bttnInscribir_Click(object sender, EventArgs e)
+        {
+            ClaseConexion con = new ClaseConexion();
+
+            if (cmbTorneo.SelectedIndex == -1 || cmbEquipo.SelectedIndex == -1)
+            {
+                MessageBox.Show("Debe seleccionar un torneo y un equipo");
+                return;
+            }
+
+            using (SqlConnection conn = con.conectar())
+            {
+                conn.Open();
+
+                SqlTransaction trans = conn.BeginTransaction();
+
+                try
+                {
+                    int idTorneo = Convert.ToInt32(cmbTorneo.SelectedValue);
+                    int idEquipo = Convert.ToInt32(cmbEquipo.SelectedValue);
+
+                    // 1️⃣ VALIDAR SI EL EQUIPO YA ESTA INSCRITO
+                    string validar = @"SELECT COUNT(*)
+                               FROM DetalleTorneo
+                               WHERE IdTorneo=@torneo AND IdEquipo=@equipo";
+
+                    SqlCommand cmdValidar = new SqlCommand(validar, conn, trans);
+
+                    cmdValidar.Parameters.AddWithValue("@torneo", idTorneo);
+                    cmdValidar.Parameters.AddWithValue("@equipo", idEquipo);
+
+                    int existe = (int)cmdValidar.ExecuteScalar();
+
+                    if (existe > 0)
+                    {
+                        MessageBox.Show("Ese equipo ya está inscrito en el torneo");
+                        trans.Rollback();
+                        return;
+                    }
+
+                    // 2️⃣ VALIDAR NOMBRE DE EQUIPO DUPLICADO
+                    string validarNombre = @"
+                    SELECT COUNT(*)
+                    FROM DetalleTorneo DT
+                    INNER JOIN Equipo E ON DT.IdEquipo = E.IdEquipo
+                    WHERE DT.IdTorneo=@torneo AND E.NombreEquipo =
+                    (SELECT NombreEquipo FROM Equipo WHERE IdEquipo=@equipo)";
+
+                    SqlCommand cmdNombre = new SqlCommand(validarNombre, conn, trans);
+
+                    cmdNombre.Parameters.AddWithValue("@torneo", idTorneo);
+                    cmdNombre.Parameters.AddWithValue("@equipo", idEquipo);
+
+                    int nombreExiste = (int)cmdNombre.ExecuteScalar();
+
+                    if (nombreExiste > 0)
+                    {
+                        MessageBox.Show("Ya existe un equipo con ese nombre en el torneo");
+                        trans.Rollback();
+                        return;
+                    }
+
+                    // 3️⃣ INSERTAR EQUIPO
+                    string insert = @"INSERT INTO DetalleTorneo(IdTorneo,IdEquipo)
+                              VALUES(@torneo,@equipo)";
+
+                    SqlCommand cmdInsert = new SqlCommand(insert, conn, trans);
+
+                    cmdInsert.Parameters.AddWithValue("@torneo", idTorneo);
+                    cmdInsert.Parameters.AddWithValue("@equipo", idEquipo);
+
+                    cmdInsert.ExecuteNonQuery();
+
+                    // 4️⃣ CONTAR EQUIPOS
+                    string count = @"SELECT COUNT(*)
+                             FROM DetalleTorneo
+                             WHERE IdTorneo=@torneo";
+
+                    SqlCommand cmdCount = new SqlCommand(count, conn, trans);
+
+                    cmdCount.Parameters.AddWithValue("@torneo", idTorneo);
+
+                    int totalEquipos = (int)cmdCount.ExecuteScalar();
+
+                    // 5️⃣ CALCULAR JORNADAS
+                    int jornadas = 0;
+
+                    if (totalEquipos > 1)
+                    {
+                        jornadas = totalEquipos - 1;
+                    }
+                    // 6️⃣ ACTUALIZAR TORNEO
+                    string update = @"UPDATE Torneo
+                              SET CantEquipos=@equipos,
+                                  NumJornadas=@jornadas
+                              WHERE IdTorneo=@torneo";
+
+                    SqlCommand cmdUpdate = new SqlCommand(update, conn, trans);
+
+                    cmdUpdate.Parameters.AddWithValue("@equipos", totalEquipos);
+                    cmdUpdate.Parameters.AddWithValue("@jornadas", jornadas);
+                    cmdUpdate.Parameters.AddWithValue("@torneo", idTorneo);
+
+                    cmdUpdate.ExecuteNonQuery();
+
+                    trans.Commit();
+                    cmbEquipo.SelectedIndex = -1;
+                    CargarEquipos();
+                    CargarDetalleTorneo();
+
+                    MessageBox.Show("Equipo inscrito correctamente");
+                }
+                catch (Exception ex)
+                {
+                    trans.Rollback();
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void cmbTorneo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbTorneo.SelectedValue == null)
+                return;
+        }
+
+        private void cmbEquipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbEquipo.SelectedValue == null)
+                return;
+        }
+
+        void CargarDetalleTorneo()
+        {
+            ClaseConexion con = new ClaseConexion();
+
+            using (SqlConnection conn = con.conectar())
+            {
+                conn.Open();
+
+                string query = @"SELECT DT.IdTorneo,
+                 DT.IdEquipo,
+                 T.NombreTorneo,
+                 E.NombreEquipo
+                 FROM DetalleTorneo DT
+                 INNER JOIN Torneo T ON DT.IdTorneo = T.IdTorneo
+                 INNER JOIN Equipo E ON DT.IdEquipo = E.IdEquipo";
+
+
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+
+                da.Fill(dt);
+
+                dgvDetalleTorneo.DataSource = dt;
+            }
+        }
+
+        private void bttnModificar_Click(object sender, EventArgs e)
+        {
+            ClaseConexion con = new ClaseConexion();
+            if (dgvDetalleTorneo.CurrentRow == null)
+            {
+                MessageBox.Show("Seleccione un registro");
+                return;
+            }
+
+            using (SqlConnection conn = con.conectar())
+            {
+                conn.Open();
+
+                int idTorneo = Convert.ToInt32(dgvDetalleTorneo.CurrentRow.Cells["IdTorneo"].Value);
+                int idEquipo = Convert.ToInt32(dgvDetalleTorneo.CurrentRow.Cells["IdEquipo"].Value);
+
+                string query = @"UPDATE DetalleTorneo
+                         SET IdTorneo=@torneoNuevo,
+                             IdEquipo=@equipoNuevo
+                         WHERE IdTorneo=@torneo AND IdEquipo=@equipo";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@torneoNuevo", cmbTorneo.SelectedValue);
+                cmd.Parameters.AddWithValue("@equipoNuevo", cmbEquipo.SelectedValue);
+                cmd.Parameters.AddWithValue("@torneo", idTorneo);
+                cmd.Parameters.AddWithValue("@equipo", idEquipo);
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Registro modificado");
+
+                CargarDetalleTorneo();
+            }
+        }
+
+        private void bttnEliminar_Click(object sender, EventArgs e)
+        {
+            ClaseConexion con = new ClaseConexion();
+            if (dgvDetalleTorneo.CurrentRow == null)
+            {
+                MessageBox.Show("Seleccione un registro");
+                return;
+            }
+            using (SqlConnection conn = con.conectar())
+            {
+                conn.Open();
+
+                int idTorneo = Convert.ToInt32(dgvDetalleTorneo.CurrentRow.Cells["IdTorneo"].Value);
+                int idEquipo = Convert.ToInt32(dgvDetalleTorneo.CurrentRow.Cells["IdEquipo"].Value);
+
+                string query = @"DELETE FROM DetalleTorneo
+                         WHERE IdTorneo=@torneo AND IdEquipo=@equipo";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+
+                cmd.Parameters.AddWithValue("@torneo", idTorneo);
+                cmd.Parameters.AddWithValue("@equipo", idEquipo);
+
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Registro eliminado");
+
+                CargarDetalleTorneo();
+            }
+        }
+    }
+}
